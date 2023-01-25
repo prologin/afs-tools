bfad61e1fa32e45a3bab036d6cda467b2ae58db4
Marc 'risson' Schmitt <marc.schmitt@prologin.org>
2023-01-25 02:40:28 +0100

    Initial commit
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@prologin.org>

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+FROM python:3.10-slim as base
+
+ENV DEBIAN_FRONTEND=noninteractive \
+    LC_ALL=C.UTF-8 \
+    LANG=C.UTF-8 \
+    PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONFAULTHANDLER=1 \
+    PYTHONHASHSEED=random \
+    PYTHONUNBUFFERED=1 \
+    POETRY_VIRTUALENVS_CREATE=false \
+    VENV_PATH="/opt/venv" \
+    PATH="/opt/venv/bin:$PATH"
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        git make \
+        curl ca-certificates gnupg apt-transport-https
+
+RUN curl --location \
+        --output /usr/local/bin/release-cli \
+        "https://gitlab.com/api/v4/projects/gitlab-org%2Frelease-cli/packages/generic/release-cli/latest/release-cli-linux-amd64" && \
+    chmod +x /usr/local/bin/release-cli
+
+ARG INCLUDE_DEV_DEPS=false
+
+FROM base as builder
+
+ARG INCLUDE_DEV_DEPS=false
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends gcc python-dev git
+
+RUN --mount=type=bind,target=./pyproject.toml,src=./pyproject.toml \
+    --mount=type=bind,target=./poetry.lock,src=./poetry.lock \
+    --mount=type=cache,target=/root/.cache/pypoetry \
+    python -m venv /opt/venv && \
+    pip3 install --upgrade pip && \
+    pip3 install poetry && \
+    poetry install $(if [ $INCLUDE_DEV_DEPS = "false" ]; then echo "--no-dev"; fi)
+
+COPY ./ /afs_tools
+WORKDIR /afs_tools
+RUN poetry install
+
+FROM base
+
+COPY --from=builder /opt/venv/ /opt/venv/
+COPY --from=builder /afs_tools/ /afs_tools/
+WORKDIR /
---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    futils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = { self, nixpkgs, futils, poetry2nix } @ inputs:
+    let
+      inherit (nixpkgs) lib;
+      inherit (lib) recursiveUpdate;
+      inherit (futils.lib) eachDefaultSystem defaultSystems;
+
+      nixpkgsFor = lib.genAttrs defaultSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [
+          poetry2nix.overlay
+          self.overlay
+        ];
+      });
+
+      poetryArgs = { pkgs, groups ? [ ] }: {
+        projectDir = self;
+        src = self;
+        inherit groups;
+
+        overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+          dataconf = super.dataconf.overridePythonAttrs (old: {
+            propagatedBuildInputs = old.propagatedBuildInputs ++ [ self.poetry ];
+          });
+          poetry-semver = super.poetry-semver.overridePythonAttrs (old: {
+            propagatedBuildInputs = old.propagatedBuildInputs ++ [ self.poetry ];
+          });
+          pydocstyle = super.pydocstyle.overridePythonAttrs (old: {
+            propagatedBuildInputs = old.propagatedBuildInputs ++ [ self.poetry ];
+          });
+          pyproject-hooks = super.pyproject-hooks.overridePythonAttrs (old: {
+            propagatedBuildInputs = old.propagatedBuildInputs ++ [ self.flit-core ];
+          });
+          setoptconf-tmp = super.setoptconf-tmp.overridePythonAttrs (old: {
+            buildInputs = old.buildInputs ++ [ self.setuptools ];
+          });
+        });
+
+        meta = with lib; {
+          inherit (self) description;
+          maintainers = with maintainers; [ risson ];
+        };
+      };
+
+      anySystemOutputs = {
+        overlay = final: prev: {
+          afs-tools = final.poetry2nix.mkPoetryApplication (poetryArgs { pkgs = final; });
+        };
+      };
+
+      multipleSystemsOutputs = eachDefaultSystem (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              git
+              poetry
+              ((pkgs.poetry2nix.mkPoetryEnv (removeAttrs (poetryArgs { inherit pkgs; groups = [ "dev" ]; }) [ "meta" "src" "propagatedBuildInputs" ])).override { ignoreCollisions = true; })
+            ];
+
+            PYTHONPATH = ".";
+          };
+
+          packages = {
+            inherit (pkgs) afs-tools;
+            default = pkgs.afs-tools;
+          };
+
+          apps = {
+            afs-tools = {
+              type = "app";
+              program = "${self.packages.${system}.afs-tools}/bin/afs-tools";
+            };
+            default = {
+              type = "app";
+              program = "${self.packages.${system}.default}/bin/afs-tools";
+            };
+          };
+        });
+    in
+    recursiveUpdate multipleSystemsOutputs anySystemOutputs;
+}
